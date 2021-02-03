895875205583689faf0bf72bdf02a1ec3b6e0d3f
Alan <alan.f@msn.com>
2021-02-03 20:21:58 -0300

    Enabled Django admin for user profile model

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-class HelloAPIView(APIView):
+class HelloApiView(APIView):
     """Test API View"""
 
     def get(self, request, format=None):
